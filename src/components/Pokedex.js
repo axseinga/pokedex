540565029de8dc94ba262b8ca3aedfb8ecd3aa8b
Agnieszka Urbanowicz <urbanowicz.uk@gmail.com>
2021-08-19 15:38:59 +0100

    first commit

---
+++ src/components/Pokedex.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class Pokedex extends React.Component {
+    render() {
+        return <p>bleble</p>;
+    }
+}
+
+export default Pokedex;
